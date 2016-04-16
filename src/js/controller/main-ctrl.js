970942943a25e51577895514d66d53c4f7f8342e
Gerald Austria <geraldmichaelaustria@gmail.com>
2016-04-16 13:41:36 +0800

    update v6

--- src/js/controller/main-ctrl.js
+++ src/js/controller/main-ctrl.js
@@ -1,3 +1,5 @@
-app.controller("MainCtrl",["$scope","$rootScope",function($scope,$rootScope){
-	
+app.controller("MainCtrl",["$scope","$rootScope","$location",function($scope,$rootScope,$location){
+	 $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
 }]);
