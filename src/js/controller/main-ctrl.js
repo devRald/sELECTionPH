10f827ca5f9e2d8b8e6788170b5e1a7782d1469c
Gerald Austria <geraldmichaelaustria@gmail.com>
2016-04-16 14:34:03 +0800

    update v8

--- src/js/controller/main-ctrl.js
+++ src/js/controller/main-ctrl.js
@@ -1,5 +1,14 @@
-app.controller("MainCtrl",["$scope","$rootScope","$location",function($scope,$rootScope,$location){
+app.controller("MainCtrl",["$scope","$rootScope","$location","$http",function($scope,$rootScope,$location,$http){
+	 var key = "H5qWcTYm5" , token = "hmXtL0GEKjG5WFdN68pYOKBSU1oq";
 	 $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
+
+    $scope.testData = function(){
+    	$http.get("http://api.bilangpilipino.com/api-bilang-pilipino/api/candidates/2016?key=H5qWcTYm5&token=hmXtL0GEKjG5WFdN68pYOKBSU1oq").then(function(response){
+    		
+    	});
+    }
+
+    $scope.testData();
 }]);
